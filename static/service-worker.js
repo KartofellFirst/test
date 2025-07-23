c233d7000a82e769d19dc6cee417d489540a2124
KartofellFirst <deznevnikita8@gmail.com>
2025-07-24 00:22:12 +0300

    Update service-worker.js

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,14 +1,8 @@
 self.addEventListener("install", (e) => {
   e.waitUntil(
     caches.open("nt-cache").then((cache) =>
-      cache.addAll(["/", "/static/index.html",  "/static/icon-192.png", "/static/icon-512.png"])
+      cache.addAll(["/", "/static/index.html", "/static/fallback.html", "/static/icon-192.png", "/static/icon-512.png"])
     )
-  );
-});
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
 
@@ -16,7 +10,7 @@ self.addEventListener("fetch", (e) => {
 self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then((res) => {
-      return res || fetch(e.request).catch(() => caches.match("/static/index.html"));
+      return res || fetch(e.request).catch(() => caches.match("/static/fallback.html"));
     })
   );
 });
